893467e775541484661f4c3277299be269620edd
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-03-05 07:40:51 +0000

    Identity service , server.js

--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -1,1 +1,91 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const express = require("express");
+const hemlet = require("helmet");
+const { RateLimiterRedis } = require("rate-limiter-flexible");
+const Redis = require("ioredis");
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis");
+const routes = require("./routes/identity-service");
+const errorHandler = require("./middleware/error-handler");
 
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//connect to database
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB:", error);
+  });
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(hemlet());
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body: ${JSON.stringify(req.body)}`);
+  next();
+});
+
+//DDOS protection and rate limiting
+
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 12,
+  duration: 1,
+});
+
+app.use((req, res, next) => {
+  rateLimiter
+    .consume(req.ip)
+    .then(() => next())
+    .catch(() => {
+      logger.warn(`Rate limit for IP ${req.ip} exceeded`);
+      res.status(429).send("Too many requests");
+    });
+});
+
+//ip based rate limiting for sensitive endpoints
+const sensitiveEndpointLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 25,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Rate limit for IP ${req.ip} exceeded on sensitive endpoint`);
+    res.status(429).send("Too many requests");
+  },
+  store: new Redis({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+//apply sensitive endpoint rate limiting to sensitive endpoints
+app.use("/api/auth/register", sensitiveEndpointLimiter);
+
+//routes
+app.use("/api/auth", routes);
+
+//error handler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`Identity service running on port ${PORT}`);
+});
+
+//unhandled promise rejection
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("Unhandled rejection at :", promise, "reason:", reason);
+  // process.exit(1);
+});
