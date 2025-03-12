b794bd1f0a8e6039a8c5ba00880b6beb752ec65a
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-03-12 02:31:31 +0000

    get all post , Post-Service

--- post-service/src/controllers/post-controller.js
+++ post-service/src/controllers/post-controller.js
@@ -2,7 +2,6 @@ const Post = require("../models/Post");
 const logger = require("../utils/logger");
 const { validatePost } = require("../utils/validation");
 
-
 const createPost = async (req, res) => {
   logger.info("Create Post endpoint hit...");
   try {
@@ -40,6 +39,33 @@ const createPost = async (req, res) => {
 
 const getAllPost = async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const startIndex = (page - 1) * 10;
+
+    const cacheKey = `posts:${page}:${limit}`;
+    const cachedPost = await req.redisClient.get(cacheKey);
+    if (cachedPost) {
+      return res.json(JSON.parse(cachedPost));
+    }
+
+    const posts = await Post.find({})
+      .sort({ createdAt: -1 })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalNoOfPosts = await Post.countDocuments();
+    const result = {
+      posts,
+      currentpage: page,
+      totalPages: Math.ceil(totalNoOfPosts / limit),
+      totalPosts: totalNoOfPosts,
+    };
+    //save post in redis cache
+    await req.redisClient.setex(cacheKey, 300, JSON.stringify(result));
+
+    res.json(result);
   } catch (error) {
     logger.error("Error Fetching post", error);
     res.status(500).json({
--- post-service/src/routes/post-routes.js
+++ post-service/src/routes/post-routes.js
@@ -1,5 +1,5 @@
 const express  = require('express')
-const { createPost } = require('../controllers/post-controller')
+const { createPost, getAllPost } = require('../controllers/post-controller')
 const { authenticateRequest } = require('../middleware/authMiddleware')
 const router = express.Router()
 
@@ -8,5 +8,6 @@ const router = express.Router()
 router.use(authenticateRequest)
 
 router.post('/create-post',createPost)
+router.get('/all-posts',getAllPost)
 
 module.exports = router
