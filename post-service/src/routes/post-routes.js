bf1a7a2aaf776d7570ded0c17fea47936065f4e2
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-03-12 10:01:35 +0000

    delete working and cache working well

--- post-service/src/routes/post-routes.js
+++ post-service/src/routes/post-routes.js
@@ -1,5 +1,5 @@
 const express  = require('express')
-const { createPost, getAllPost, getPost } = require('../controllers/post-controller')
+const { createPost, getAllPost, getPost, deletePost } = require('../controllers/post-controller')
 const { authenticateRequest } = require('../middleware/authMiddleware')
 const router = express.Router()
 
@@ -10,5 +10,6 @@ router.use(authenticateRequest)
 router.post('/create-post',createPost)
 router.get('/all-posts',getAllPost)
 router.get('/:id',getPost)
+router.delete('/:id',deletePost)
 
 module.exports = router
