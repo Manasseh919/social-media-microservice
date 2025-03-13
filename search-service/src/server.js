de64ca68f1e364f1fd7e82aae10ce465b26dfc64
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-03-13 22:56:16 +0000

    search server done

--- search-service/src/server.js
+++ search-service/src/server.js
@@ -6,9 +6,10 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { rateLimit } = require("express-rate-limit");
 const { RedisStore } = require("rate-limit-redis");
-const postRoutes = require("./routes/post-routes");
+// const se = require("./routes/post-routes");
 const errorHandler = require("./middleware/errorHandler");
 const logger = require("./utils/logger");
+const searchRoutes = require("./routes/searchRoutes");
 
 const app = express();
 
@@ -29,7 +30,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-    logger.info(`Received ${req.method} request to ${req.url}`);
-    logger.info(`Request body, ${req.body}`);
-    next();
-  });
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body, ${req.body}`);
+  next();
+});
+
+//routes
+app.use("/api/search", searchRoutes);
