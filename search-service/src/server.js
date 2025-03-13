20dc8c68ccb5156b66de38cefad63089069dff01
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2025-03-13 22:53:06 +0000

    Search endpoint created

---
+++ search-service/src/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Redis = require("ioredis");
+const cors = require("cors");
+const helmet = require("helmet");
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis");
+const postRoutes = require("./routes/post-routes");
+const errorHandler = require("./middleware/errorHandler");
+const logger = require("./utils/logger");
+
+const app = express();
+
+const PORT = process.env.PORT || 3004;
+
+//connect to database
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB:", error);
+  });
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    logger.info(`Received ${req.method} request to ${req.url}`);
+    logger.info(`Request body, ${req.body}`);
+    next();
+  });
